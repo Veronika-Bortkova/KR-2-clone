d000086f96ed9d2399d6141d18b2e30106d64cb5
Veronika-Bortkova <shishiga79@gmail.com>
2026-02-24 23:15:12 +0200

    1 d

---
+++ main.js
@@ -0,0 +1,17 @@
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then((users) => {
+        console.log(users);
+        let arrDivBloks = Array.from(document.getElementsByClassName("block"));
+           arrDivBloks.forEach((divBlock, index) =>{
+               if (users[index]) {
+               let divId = document.createElement("div");
+               divId.className = "name";
+               let divName = document.createElement("div");
+               divName.className = "name";
+               divId.innerText = users[index].id;
+               divName.innerText = users[index].name;
+               divBlock.append(divId, divName);
+               }
+           });
+    });
