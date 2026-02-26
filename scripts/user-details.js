dc3218b614f72464eb0b9336571b9f6cefb0bda1
Veronika-Bortkova <shishiga79@gmail.com>
2026-02-26 23:18:13 +0200

    3 d

--- scripts/user-details.js
+++ scripts/user-details.js
@@ -84,8 +84,7 @@ fetch(`https://jsonplaceholder.typicode.com/users/${id}/posts`)
             divPostTitle.innerText ="Post title: " + firstСapitalLettere(posts.title);
             let buttonPostDetalis = document.createElement("a");
             buttonPostDetalis.className = "buttonPostDetalis";
-            buttonPostDetalis.href = "pages/post-details.html"
-                // ?" + posts.id;
+            buttonPostDetalis.href = "post-details.html?id=" + posts.id;
             buttonPostDetalis.innerText = "Post details";
             divPostWrapper.append(divPostTitle, buttonPostDetalis);
             divWrapper.append(divPostWrapper);
